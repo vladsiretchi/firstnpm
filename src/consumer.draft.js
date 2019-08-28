23eba70b299805229a1e211135763668e9e8b365
Vlad Siretchi <vlad.siretchi@hotmail.com>
2019-08-28 17:37:45 +0300

    feat: initial commit
    initial commit of first npm package

---
+++ src/consumer.draft.js
@@ -0,0 +1,10 @@
+const log = require("@ajar/marker");
+const slugger = require("./index")
+
+function main() {
+    var result = slugger("Test the slugger function");
+    log.blue(result);
+}
+
+main();
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+function slugger(...args) 
+{
+    var result = args.map(item=> {
+        return item.replace(/\s/g, '-')
+    });
+
+    return result;
+}
+module.exports = slugger;
+// slugger("I am a slugger function");
